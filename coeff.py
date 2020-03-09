554e458169c21f8c2c6fb778a309aecfa22ff591
Nikita <basynin.nikita@gmail.com>
2020-03-09 19:44:47 +0300

    alfa version: need add

--- coeff.py
+++ coeff.py
@@ -1,5 +1,7 @@
 import sys
 import itertools
+from collections import defaultdict
+import re
 
 
 def formCoefVect():
@@ -12,7 +14,7 @@ def formCoefVect():
                 j -= 1
                 break
         j += 1
-    cf.remove(cf[0])
+    #cf.remove(cf[0])
     coef_vect = list()
     for el in cf:
         coef = ""
@@ -23,6 +25,8 @@ def formCoefVect():
                 coef += str(el[i])
         coef += str(el[len(el) - 1])
         coef_vect.append(coef)
+        coef_vect.append(coef[1:])
+    coef_vect.remove(coef_vect[1])
     return coef_vect
 
 
@@ -36,16 +40,19 @@ def findBadMonoms(values, dnf, cf):  # , cf, dnf):
             s = "| "
             for i in coef:
                 if elstr[5 - int(i)] == '0':
-                    s += '+' + str(i)
-                else:
                     s += '+/' + str(i)
+                else:
+                    s += '+' + str(i)
             s += ' |'
             bad.append(s)
     return bad
 
 
 def minim(values, dnf, cf, bad):
+    numb = defaultdict(int)
+    system = list()
     for el in values:
+        ur = list()
         out = ''
         elstr = ''.join(str(x) for x in el)
         if elstr not in dnf:
@@ -54,16 +61,46 @@ def minim(values, dnf, cf, bad):
             s = "| "
             for i in coef:
                 if elstr[5 - int(i)] == '0':
-                    s += '+' + str(i)
-                else:
                     s += '+/' + str(i)
+                else:
+                    s += '+' + str(i)
             s += ' |'
             if s in bad:
                 continue
             else:
+                ur.append(s)
+                a = numb.setdefault(s, 0)
+                a += 1
+                numb[s] = a
                 out += s + '+'
+        system.append(ur)
         out = out[:-1]
-        print(out + '=1 for' + str(el) + '\n')
+        print(out + '=1 for' + str(el))
+    #sred = sorted(numb.items(), key=lambda value: value[1], reverse=True)
+    #sred.sort(key=lambda k: len(re.sub(r"[+/|]", "", k[0])))
+    sred = sorted(numb.items(), key=lambda k: len(re.sub(r"[+/|]", "", k[0])))
+    sred.sort(key=lambda k: k[1],reverse=True)
+    needed_coef = list()
+    i = 0
+    while system:
+        for ur in system:
+            if sred[i][0] in ur:
+                needed_coef.append(sred[i][0])
+                j = 0
+                while j < len(system):
+                    if sred[i][0] in system[j]:
+                        system.remove(system[j])
+                    else:
+                        j += 1
+
+                # for uravn in system:
+                #     for coeff in uravn:
+                #         a = numb.setdefault(coeff, 0)
+                #         a += 1
+                #         numb[coeff] = a
+                break
+        i += 1
+    return needed_coef
 
 
 dnf = list()
@@ -73,6 +110,5 @@ for st in sys.stdin:
 n = 6
 cf = formCoefVect()
 values = list(itertools.product(range(2), repeat=6))
-
 bad = findBadMonoms(values, dnf, cf)
 minim(values, dnf, cf, bad)
